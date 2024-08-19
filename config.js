31c2ad2b064a0170bfb0e151ebef829f601554ca
Felipe Reyes Sanchez <jfelipe9.121@gmail.com>
2024-08-18 21:23:10 -0500

    Deploy

---
+++ config.js
@@ -0,0 +1,12 @@
+
+var config = {
+    texts: [
+        "Hola mi amor solo queria decirte",      
+        "que TE AMO, eres mejor que me ha pasado",
+        "Me gustas mucho mucho",
+        "no existen palabras para expresar lo que siento por ti",
+        "simplemente eres lo mejor que me ha podido pasar",
+    ],
+    wish: "",
+    time: '2023-12-21T00:00:00',
+};
